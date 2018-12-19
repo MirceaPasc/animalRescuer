a5d105b01aba5746d9336b3beff44c8a73bd06c5
MirceaPasc <cristy_pasc@yahoo.com>
2018-12-20 01:37:09 +0200

    .

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,33 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Animal dog = new Animal();
+        dog.name = "Grigore"
+        dog.age = 3;
+        dog.health ;
+
+        Food food = new Food();
+        food.availableInStock = true;
+        food.expirationDate = 22.03.2019;
+        food.name = "carne";
+        food.price = 10;
+        food.quantity = 4;
+
+        Adopter man = new Adopter();
+        man.money = 1000;
+        man.name = "Mirea";
+
+        RecreationalActivities sport = new RecreationalActivities();
+        sport.name = "aport";
+
+        Vet doctor = new Vet();
+        doctor.name = "Andrei";
+        doctor.veterinarySpecialization = "dogs";
+
+
     }
 }
