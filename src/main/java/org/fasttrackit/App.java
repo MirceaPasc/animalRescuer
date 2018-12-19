81c82b67f3679a647376b9b78df7d85189223271
MirceaPasc <cristy_pasc@yahoo.com>
2018-12-20 01:51:10 +0200

    .

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -24,7 +24,6 @@ public class App
         RecreationalActivities sport = new RecreationalActivities();
         sport.name = "aport";
 
-
         Vet doctor = new Vet();
         doctor.name = "Andrei";
         doctor.veterinarySpecialization = "dogs";
